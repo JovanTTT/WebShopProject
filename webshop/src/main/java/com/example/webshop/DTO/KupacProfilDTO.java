a003b391601b50ae6fdd39b7eaef2b11b8cd97b5
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-05-27 18:23:14 +0200

    Dodato jos DTO klasa

---
+++ webshop/src/main/java/com/example/webshop/DTO/KupacProfilDTO.java
@@ -0,0 +1,38 @@
+package com.example.webshop.DTO;
+
+
+import com.example.webshop.model.Recenzija;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KupacProfilDTO {
+
+    private String ime;
+
+    private String prezime;
+
+    private String korisnickoIme;
+
+    private Date datumRodjenja;
+
+    private String slika;
+
+    private String opisKorisnika;
+
+    private Double prosecnaOcena;
+
+    private Set<ProizvodiNaProdajuDTO> kupljeniProizvodi = new HashSet<>();
+
+    private Set<Recenzija> dobijeneRecenzije = new HashSet<>();
+
+}
