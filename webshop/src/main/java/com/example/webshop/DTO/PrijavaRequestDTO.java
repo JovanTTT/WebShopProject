8d3dbe4ff7025ea8f085c602e78650346822dbf0
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-05-28 15:25:22 +0200

    DTO

---
+++ webshop/src/main/java/com/example/webshop/DTO/PrijavaRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.webshop.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PrijavaRequestDTO {
+
+    private String razlogPrijave;
+}
