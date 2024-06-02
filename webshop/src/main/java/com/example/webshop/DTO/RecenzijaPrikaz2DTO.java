bb956f7b312bd1a2f47002ee0a89f1b42934266d
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-06-02 11:20:11 +0200

    DTO

---
+++ webshop/src/main/java/com/example/webshop/DTO/RecenzijaPrikaz2DTO.java
@@ -0,0 +1,24 @@
+package com.example.webshop.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecenzijaPrikaz2DTO {
+
+    private KupacPrikazRecenzijeDTO kupacKojemSamDaoRecenziju;
+
+    private String komentar;
+
+    private Date datumPodnosenjaRecenzije;
+
+    private Double ocena;
+
+}
