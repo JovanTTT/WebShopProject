412a994f8202ad30f0b7b8fab16e7fd2928ba17e
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-06-02 11:19:19 +0200

    DTO

---
+++ webshop/src/main/java/com/example/webshop/DTO/ProizvodPrekoKategorijeDTO.java
@@ -0,0 +1,19 @@
+package com.example.webshop.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProizvodPrekoKategorijeDTO {
+
+    private String naziv;
+
+    private String opis;
+
+    private String slikaProizvoda;
+}
