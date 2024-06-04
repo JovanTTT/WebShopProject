f009eae791e977db9b3033ef8a073baff65ebbc8
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-06-04 22:20:23 +0200

    DTO

---
+++ webshop/src/main/java/com/example/webshop/DTO/SviProizvodiDTO.java
@@ -0,0 +1,30 @@
+package com.example.webshop.DTO;
+
+import com.example.webshop.model.TipProdaje;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SviProizvodiDTO {
+    private Long id;
+
+    private String naziv;
+
+    private String opis;
+
+    private String slikaProizvoda;
+
+    private Set<KategorijaDTO> kategorije = new HashSet<>();
+
+    private Double cena;
+
+    private TipProdaje tipProdaje;
+}
