e449e9efa466d6aba79745c4595dd6ca276265f6
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-06-05 21:25:36 +0200

    Error

---
+++ webshop/src/main/java/com/example/webshop/error/CategoryExistsException.java
@@ -0,0 +1,12 @@
+package com.example.webshop.error;
+
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CategoryExistsException extends BadRequestException {
+    public CategoryExistsException(String massage) {
+        super(massage);
+    }
+}
