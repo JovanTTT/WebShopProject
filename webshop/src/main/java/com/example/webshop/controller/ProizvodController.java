597c790214ceebb3a07a76ed75383127ef3e9113
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-06-22 12:48:24 +0200

    getAllProducts controller

--- webshop/src/main/java/com/example/webshop/controller/ProizvodController.java
+++ webshop/src/main/java/com/example/webshop/controller/ProizvodController.java
@@ -1,9 +1,27 @@
 package com.example.webshop.controller;
 
+import com.example.webshop.DTO.SviProizvodiDTO;
+import com.example.webshop.service.ProizvodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api/product")
 public class ProizvodController {
+
+    @Autowired
+    private ProizvodService proizvodService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<SviProizvodiDTO>> getAllProducts() {
+
+        List<SviProizvodiDTO> proizvodiDTO = proizvodService.findAllProducts();
+        return ResponseEntity.ok(proizvodiDTO);
+    }
+
 }
