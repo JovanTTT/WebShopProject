cde6e7552d83f7b789db49e339a9d26dbabd3da0
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-06-28 14:59:18 +0200

    findAll service

--- webshop/src/main/java/com/example/webshop/service/KategorijaService.java
+++ webshop/src/main/java/com/example/webshop/service/KategorijaService.java
@@ -1,9 +1,13 @@
 package com.example.webshop.service;
 
+import com.example.webshop.DTO.KategorijaDTO;
 import com.example.webshop.model.Kategorija;
 import com.example.webshop.repository.KategorijaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class KategorijaService {
@@ -23,5 +27,21 @@ public class KategorijaService {
         novaKategorija.setNazivKategorije(nazivKategorije);
 
         kategorijaRepository.save(novaKategorija);
+    }
+
+    public List<KategorijaDTO> findAll() {
+
+        List<Kategorija>kategorije= kategorijaRepository.findAll();
+        List<KategorijaDTO>kategorijeDTO=new ArrayList<>();
+        List<Kategorija> slanje=new ArrayList<>();
+
+        for(Kategorija k:kategorije){
+            KategorijaDTO kategorijaDTO=new KategorijaDTO();
+            kategorijaDTO.setNazivKategorije(k.getNazivKategorije());
+            // kategorijaDTO.setId(k.getId());
+            kategorijeDTO.add(kategorijaDTO);
+        }
+
+        return kategorijeDTO;
     }
 }
