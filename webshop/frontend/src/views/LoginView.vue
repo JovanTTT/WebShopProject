463b9ced2adb077fe76abcb42372257cc575028e
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-07-03 15:56:20 +0200

    LoginView script

--- webshop/frontend/src/views/LoginView.vue
+++ webshop/frontend/src/views/LoginView.vue
@@ -10,7 +10,42 @@
 </template>
 
 <script>
+import axios from "axios";
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: "LoginView",
+  components: {HelloWorld},
+  data: function () {
+    return {
+      korisnik: {},
+    };
+  },
+  methods: {
+    login: function () {
+      axios
+          .post("http://localhost:8080/api/user/login", this.korisnik, {
+            withCredentials: true,//zbog sesije
+          })
+          .then((res) => {
+            console.log("Prijavljen korisnik:", this.korisnik.korisnickoIme);
+            localStorage.setItem("user", JSON.stringify(res.data.id));
+            localStorage.setItem("userUSER", JSON.stringify(res.data));
+            localStorage.setItem("userRole", res.data.uloga); // Čuvanje uloge korisnik
+            if(res.data.uloga === 'ADMINISTRATOR'){
+              this.$router.push('/admin');
+            }else{
+              this.$router.push("/");
+            }
+            this.$emit("userLoggedIn");
+          })
+          .catch((err) => {
+            console.log(err);
+            alert("Došlo je do greške");
+          });
 
+    },
+  },
+};
 </script>
 
 <style>
