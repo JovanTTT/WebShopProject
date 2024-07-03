16a144bd534958f954dfba99c2a14141b1b8af36
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-07-03 15:55:21 +0200

    Login view html

--- webshop/frontend/src/router/index.js
+++ webshop/frontend/src/router/index.js
@@ -1,5 +1,20 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from "@/views/LoginView.vue";
+// import ProductView from "@/views/ProductView.vue";
+// import RegistrationView from "@/views/RegistrationView.vue";
+// import HomePageView from "@/views/HomePageView.vue";
+// import ProfileView from "@/views/ProfileView.vue";
+// import SellerProfileView from "@/views/SellerProfileView.vue";
+// import RecenzijeView from "@/views/RecenzijeView.vue";
+// import CustomerProfileView from "@/views/CustomerProfileView.vue";
+// import AdminView from "@/views/AdminView.vue";
+
+
+
+
+Vue.use(VueRouter)
 
 const routes = [
   {
@@ -7,19 +22,62 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
   // {
-  //   path: '/about',
-  //   name: 'about',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  //   path: '/registration',
+  //   name: 'registration',
+  //   component: RegistrationView
+  //   //neibtno
+  // },
+  // {
+  //   path: '/homepage',
+  //   name: 'homepage',
+  //   component: HomePageView
+  // },
+  // {
+  //   path: '/profile',
+  //   name: 'profile',
+  //   component: ProfileView
+  // },
+  // {
+  //   path: '/admin',
+  //   name: 'admin',
+  //   component: AdminView
+  // },
+  // // {
+  // //   path: '/profile/:id', // Definicija dinamičke rute za detalje proizvoda
+  // //   name: 'profile',
+  // //   component: ProfileView,
+  // //   props: true // Prosleđivanje parametara kao propertija komponenti
+  // // },
+  // {
+  //   path: '/product/:id', // Definicija dinamičke rute za detalje proizvoda
+  //   name: 'product',
+  //   component: ProductView,
+  //   props: true // Prosleđivanje parametara kao propertija komponenti
+  // },
+  // {
+  //   path: '/sellerProfile/:id',
+  //   name: 'sellerProfile',
+  //   component: SellerProfileView,
+  //   props: true
+  // },
+  // {
+  //   path: '/customerProfile/:id',
+  //   name: 'customerProfile',
+  //   component: CustomerProfileView,
+  //   props: true
   // }
 
 ]
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
---
+++ webshop/frontend/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <HelloWorld />
+    <div class="login-form">
+      <input v-model="korisnik.korisnickoIme" type="text" placeholder="Korisničko ime" /><br />
+      <input v-model="korisnik.lozinka" type="password" placeholder="Lozinka" /><br />
+      <button v-on:click="login">Prijavi se</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
