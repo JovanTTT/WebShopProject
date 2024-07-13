68bdb6d7a34d7e9a38f1f0ad8dbf6d2e362815f7
Jovan Trkulja <jovantrkulja003@gmail.com>
2024-07-13 22:30:39 +0200

    ProductView css

--- webshop/frontend/src/views/ProductView.vue
+++ webshop/frontend/src/views/ProductView.vue
@@ -202,5 +202,127 @@ export default {
 </script>
 
 <style scoped>
+.btn-close {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  color: rgba(47, 128, 102, 0.76);
+  cursor: pointer;
+  background: transparent;
+  border: none;
+  font-size: 24px;
+  font-weight: bold;
+  outline: none;
+  z-index: 1000;  /*Osigurava da je dugme iznad drugih elemenata */
+}
 
+.btn-close:hover {
+  color: red; /* Boja dugmeta pri prelasku mišem */
+}
+.product-details {
+  font-family: Arial;
+  display: flex;
+  margin-top: 50px; /* Razmak na vrhu */
+  margin-right: 50px;
+}
+
+.product-image {
+  margin-left: 50px;
+  flex: 0 0 auto; /* Skroz levo */
+  margin-right: 20px; /* Razmak između slike i informacija */
+  max-width: 500px; /* Maksimalna širina slike */
+}
+
+.product-info {
+  flex: 1; /* Ostatak prostora zauzima informacije */
+  font-size: 30px; /* Primer uvećanja fonta na 18px */
+}
+
+.product-image img {
+  max-width: 100%; /* Slika se skalira na maksimalnu širinu */
+  height: auto; /* Visina se automatski prilagođava */
+}
+
+.product-info h1 {
+  font-size: 24px;
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+  color: #000000;
+
+}
+
+.product-info p {
+  font-size: 18px;
+  line-height: 1.6;
+  margin-bottom: 10px;
+  justify-content: center; /* Centriranje po vertikali */
+}
+
+.buy-button-container {
+  display: flex;
+  justify-content: flex-end; /* Poravnanje na kraj */
+  align-items: flex-end; /* Poravnanje na dno */
+  margin-top: 150px; /* Pomera kontejner na dno, ako je potrebno */
+  margin-right: 300px; /* Margina za razmak od desnog kraja */
+  margin-bottom: 400px; /* Margina za razmak od donjeg kraja */
+}
+.my-custom-button {
+
+  width: 300px; /* Širina dugmeta */
+  cursor: pointer;
+  border-radius: 200px;
+  background-color: rgba(47, 128, 102, 0.76);
+}
+.my-custom-button:hover{
+  background-color: rgb(72, 136, 113);
+}
+.brand {
+  font-family: Arial;
+  margin-top: 20px;
+  margin-left:1300px ;
+}
+.modal {
+  display: block; /* Prikaži modal */
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+
+}
+
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+  max-width: 300px;
+  text-align: center;
+  border-radius: 20px;
+}
+.seller-name {
+  color: #c963c9; /* Boja teksta */
+  cursor: pointer; /* Promena kursora na pokazivač */
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
 </style>
